5dd718e60c40f10085c1342e87221666e15ae8db
Abdulrahman-Alpha <107507364+Abdulrahman-Alpha@users.noreply.github.com>
2022-11-16 23:06:03 +0200

    Added

---
+++ 427A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, e, p=0, c=0;
+
+    cin >> n;
+
+    while(n--)
+    {
+        cin >> e;
+
+        if(e==-1)
+        {
+            if(p==0)
+                c++;
+            else
+                p--;
+        }
+        else
+            p += e;
+    }
+
+    cout << c;
+}
